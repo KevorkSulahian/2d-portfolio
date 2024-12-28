c7a2109b2373949782e5bf14fbe4804d0ad23ae4
Kevork <kevork@superannotate.com>
2024-12-28 17:41:53 +0400

    more updates

--- src/constants.js
+++ src/constants.js
@@ -1,23 +1,19 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
-    I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
-    Here is my <a href="https://github.com/jslegenddev" target="_blank">Github</a>!`,
-  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+  pc: `This is my PC. I work mostly in Python on LLM Agents these days.
+    Here is my <a href="https://github.com/KevorkSulahian" target="_blank">Github</a>!`,
+  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation. I also have a grad degree in Financial Engineering.`,
   "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
-  I also like to write about my journey and what I'm building. Go  to <a href="https://github.com/KevorkSulahian/ML_journey" target="_blank">my channel</a>! (Star if you like the content)
-  You'll learn how I built this portfolio you're currently playing through!`,
+  I also like to write about my journey and what I'm building. Go  to <a href="https://github.com/KevorkSulahian/ML_journey" target="_blank">my channel</a>! (Star if you like the content)`,
   tv: `That's my TV. I've been watching tech youtubers a lot recently like :
    <a href="https://www.youtube.com/@AndrejKarpathy" target="_blank">Andrej Karpathy</a>,
   <a href="https://www.youtube.com/@YannicKilcher" target="_blank">Yannic Kilcher</a>  and <a href="https://www.youtube.com/@WesRoth" target="_blank">Wes Roth</a>! You can find more at my <a href="https://github.com/KevorkSulahian/ML_journey?tab=readme-ov-file#youtube-videos-and-channels" target="_blank">github</a>!`,
   bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-  resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
-  Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
-  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
-  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
-  Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
-  library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
-  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
-  exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
+  resume: `This is my desk and on it is my resume (linkedin Profile will add a proper page soon). <a href="https://www.linkedin.com/in/kevorkysulahian/" target="_blank">Check it out?</a>
+  Contact me at kevorkysulahian@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : I have some interesting projects like <a href="https://kevorksulahian.github.io/agentic-llm-for-better-results/" target="_blank">Agentic LLM</a>`,
+  library: `There are a lot of programming books on my shelves.
+  I probably only read few of them. Who else compulsively buys technical books without ever finishing them?`,
+  exit: `If you want to exit my portfolio, just close the tab.`,
 };
