8149f7b49c553875e85cbd03a250d0ba4f9d16e8
Kevork <kevork@superannotate.com>
2024-12-28 17:34:34 +0400

    some updates

--- src/constants.js
+++ src/constants.js
@@ -6,11 +6,11 @@ export const dialogueData = {
     Here is my <a href="https://github.com/jslegenddev" target="_blank">Github</a>!`,
   "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
   "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
-  I also make game programming tutorials on YouTube. Go sub to <a href="https://youtube.com/@jslegenddev" target="_blank">my channel</a>! (If you like the content)
+  I also like to write about my journey and what I'm building. Go  to <a href="https://github.com/KevorkSulahian/ML_journey" target="_blank">my channel</a>! (Star if you like the content)
   You'll learn how I built this portfolio you're currently playing through!`,
   tv: `That's my TV. I've been watching tech youtubers a lot recently like :
-   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
-  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+   <a href="https://www.youtube.com/@AndrejKarpathy" target="_blank">Andrej Karpathy</a>,
+  <a href="https://www.youtube.com/@YannicKilcher" target="_blank">Yannic Kilcher</a>  and <a href="https://www.youtube.com/@WesRoth" target="_blank">Wes Roth</a>! You can find more at my <a href="https://github.com/KevorkSulahian/ML_journey?tab=readme-ov-file#youtube-videos-and-channels" target="_blank">github</a>!`,
   bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
   resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
   Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
